a01d943a6ce253c5efd21e7a841492d4d7843ed6
Davi Fernandes <davifncosta@gmail.com>
2023-11-22 12:32:06 -0300

    initial commit

---
+++ utils/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHN3yu9e16kusdD13PUEUa0tKQyo1IawI",
+  authDomain: "techstack-baf06.firebaseapp.com",
+  projectId: "techstack-baf06",
+  storageBucket: "techstack-baf06.appspot.com",
+  messagingSenderId: "92840814045",
+  appId: "1:92840814045:web:b505ca835d2f3f60174234"
+};
+
+
+const app = initializeApp(firebaseConfig);
