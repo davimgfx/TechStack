27b2a9ab91b4b6b5228fd89e451075b0dcaeaa4c
Davi Fernandes <davifncosta@gmail.com>
2023-11-27 10:33:25 -0300

    feat:add articles

--- js/app/index.js
+++ js/app/index.js
@@ -1,3 +1,4 @@
+// Navbar
 const userMenu = document.querySelector(".menu");
 const userLogo = document.querySelector(".user-logo");
 
@@ -9,4 +10,60 @@ document.addEventListener("click", function (event) {
   if (!userMenu.contains(event.target) && !userLogo.contains(event.target)) {
     userMenu.classList.add("none");
   }
+});
+
+
+// By the select
+const select = document.getElementById("select");
+
+select.addEventListener("change", function () {
+  const selectedTag = select.value.toLowerCase();
+
+ const articles = document.querySelectorAll(".article");
+
+
+  articles.forEach(function (article) {
+    const articleTags = article.getAttribute("data-tags").toLowerCase();
+
+    if (selectedTag === "" || articleTags === selectedTag) {
+      article.style.display = "block";
+    } else {
+      article.style.display = "none";
+    }
+  });
+});
+
+
+// Show just principals
+document.addEventListener('DOMContentLoaded', function () {
+  const principalArticlesButton = document.querySelector('.articles');
+
+  principalArticlesButton.addEventListener('click', function () {
+
+    const articles = document.querySelectorAll('.article');
+
+   
+    articles.forEach(function (article) {
+     
+      if (article.getAttribute('type') === 'principal') {
+       
+        article.style.display = 'block';
+      } else {
+        
+        article.style.display = 'none';
+      }
+    });
+  });
+});
+
+
+// Show all articles
+const resetButton = document.querySelector('.reset-button');
+
+resetButton.addEventListener('click', function () {
+  const articles = document.querySelectorAll('.article');
+
+  articles.forEach(function (article) {
+    article.style.display = 'block';
+  });
 });
